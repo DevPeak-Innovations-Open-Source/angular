c5ab5c13563be09b122c3e1ec13989ccffb432f9
ritikchauhan35 <rc4990797@gmail.com>
2025-04-18 16:57:42 +0530

    created a header

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../header/header.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [Header],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
